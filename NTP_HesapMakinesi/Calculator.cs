42d78f655026f9e1c76d8bcb217a642a2a87ae1d
Can <mccankaya45@hotmail.com>
2022-04-20 22:48:37 +0300

    3. ödev

---
+++ NTP_HesapMakinesi/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTP_HesapMakinesi
+{
+    public class Calculator
+    {
+        public void Toplama(int sayi1,int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("{0} + {1} = {2}",sayi1,sayi2,sonuc);
+        }        
+        public void Cikarma(int sayi1,int sayi2)
+        {
+            int sonuc = sayi1 - sayi2;
+            Console.WriteLine("{0} - {1} = {2}",sayi1,sayi2,sonuc);
+        }
+        public void Carpma(int sayi1,int sayi2)
+        {
+            int sonuc = sayi1 * sayi2;
+            Console.WriteLine("{0} * {1} = {2}",sayi1,sayi2, sonuc);
+        }
+        public void Bolme(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 / sayi2;
+            Console.WriteLine("{0} / {1} = {2}",sayi1,sayi2, sonuc);
+        }
+
+    }
+}
---
+++ NTP_HesapMakinesi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NTP_HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            bool durum = true;
+            while (durum==true)
+            {
+                
+                int islem,sayi1,sayi2;
+                Console.WriteLine("-- Hesap Makinesine Hoşgeldiniz --");
+
+                Console.WriteLine("Yapmak istediğiniz işlemi seçin\n" +
+                    "1. Toplama\n" +
+                    "2. Çıkarma\n" +
+                    "3. Çarpma\n" +
+                    "4. Bölme\n" +
+                    "5. Çıkış Yap\n" +
+                    "İşlem :");
+
+
+                islem = Convert.ToInt32(Console.ReadLine());
+                if (islem==5)
+                {
+                    Console.WriteLine("Program bitti.");
+                    break;
+                }
+                Console.WriteLine("Sayı 1 Girin : ");
+                sayi1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayı 2 Girin : ");
+                sayi2 = Convert.ToInt32(Console.ReadLine());
+                switch (islem)
+                {
+                    case 1:
+                        calculator.Toplama(sayi1,sayi2);
+                        break;
+                    case 2:
+                        calculator.Cikarma(sayi1,sayi2);
+                        break;
+                    case 3:
+                        calculator.Carpma(sayi1,sayi2);
+                        break;
+                    case 4:
+                        calculator.Bolme(sayi1,sayi2);
+                        break;
+                    case 5:
+                        durum = false;
+                        Console.WriteLine("Program bitti...");
+                        break;
+                    default:
+                        Console.WriteLine("Hatali Seçim");
+                        break;
+                }
+            }
+
+        }
+    }
+}
